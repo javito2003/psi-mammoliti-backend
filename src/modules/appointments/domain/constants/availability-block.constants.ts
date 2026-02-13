362183e7fb5375d8707fcf82dd9f6b24d52af769
javito2003 <javiermorenog2a@gmail.com>
2026-02-13 14:39:11 -0300

    feat: refactor professional availability handling and introduce AvailabilityBlockHours constants

--- src/modules/appointments/domain/constants/availability-block.constants.ts
+++ src/modules/appointments/domain/constants/availability-block.constants.ts
@@ -1,12 +1,6 @@
-import { AvailabilityBlock } from 'src/modules/professionals/domain/entities/professional-availability.entity';
+export { AvailabilityBlockHours } from 'src/modules/professionals/domain/constants/availability-block-hours.constants';
 
 const APPOINTMENT_DURATION_HOURS = 1;
 export const WEEK_DAYS = 7;
 
-const AvailabilityBlockHours = {
-  [AvailabilityBlock.MORNING]: { start: 8, end: 12 },
-  [AvailabilityBlock.AFTERNOON]: { start: 13, end: 17 },
-  [AvailabilityBlock.EVENING]: { start: 18, end: 22 },
-};
-
-export { APPOINTMENT_DURATION_HOURS, AvailabilityBlockHours };
+export { APPOINTMENT_DURATION_HOURS };
