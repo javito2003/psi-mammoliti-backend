d309e379f7b4593be572a405d052b09dea9100d9
javito2003 <javiermorenog2a@gmail.com>
2026-02-13 13:56:58 -0300

    feat: refactor cookie settings to use a single production check for secure and sameSite attributes

--- src/modules/auth/infrastructure/adapters/http/auth.controller.ts
+++ src/modules/auth/infrastructure/adapters/http/auth.controller.ts
@@ -37,17 +37,19 @@ export class AuthController {
     accessToken: string,
     refreshToken: string,
   ) {
+    const isProduction = process.env.NODE_ENV === 'production';
+
     response.cookie(COOKIE_NAME.ACCESS, accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: isProduction,
+      sameSite: isProduction ? 'none' : 'strict',
       maxAge: ACCESS_TOKEN_EXPIRATION_MINUTES * 60 * 1000, // 15 minutes
     });
 
     response.cookie(COOKIE_NAME.REFRESH, refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: isProduction,
+      sameSite: isProduction ? 'none' : 'strict',
       maxAge: REFRESH_TOKEN_EXPIRATION_DAYS * 24 * 60 * 60 * 1000, // 7 days
       path: '/auth/refresh',
     });
