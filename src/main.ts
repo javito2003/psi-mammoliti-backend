3730677e6fc3e5399169cbdef800824cd8acf8c6
javito2003 <javiermorenog2a@gmail.com>
2026-02-13 01:01:55 -0300

    feat: enhance logging in exception filters and bootstrap process

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupApp } from './setup-app';
+import { ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      json: true,
+      colors: true,
+    }),
+  });
 
   app.enableCors({ origin: 'http://localhost:3000', credentials: true });
 
--- src/modules/shared/infrastructure/adapter/http/filters/domain-exception.filter.ts
+++ src/modules/shared/infrastructure/adapter/http/filters/domain-exception.filter.ts
@@ -4,6 +4,7 @@ import {
   ExceptionFilter,
   HttpException,
   InternalServerErrorException,
+  Logger,
 } from '@nestjs/common';
 import { DomainError } from 'src/modules/shared/domain/base.exception';
 import { domainHttpErrorMap } from '../domain.error-map';
@@ -11,12 +12,20 @@ import { Response } from 'express';
 
 @Catch()
 export class DomainExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(DomainExceptionFilter.name, {
+    timestamp: true,
+  });
+
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Response>();
 
     if (exception instanceof HttpException) {
       const status = exception.getStatus();
+      this.logger.error(
+        `HTTP Exception: ${status} - ${exception.message}`,
+        exception.stack,
+      );
       return res.status(status).json(exception.getResponse());
     }
 
@@ -24,10 +33,20 @@ export class DomainExceptionFilter implements ExceptionFilter {
       const mapper = domainHttpErrorMap[exception.code];
       if (mapper) {
         const httpEx = mapper(exception);
+        this.logger.warn(
+          `Domain Error: ${exception.code} - ${exception.message}`,
+          exception.meta,
+        );
         return res.status(httpEx.getStatus()).json(httpEx.getResponse());
       }
     }
 
+    this.logger.error(
+      `Unexpected Exception: ${exception instanceof Error ? exception.message : String(exception)}`,
+      {
+        stack: exception instanceof Error ? exception.stack : undefined,
+      },
+    );
     const httpEx = new InternalServerErrorException();
     return res.status(httpEx.getStatus()).json(httpEx.getResponse());
   }
